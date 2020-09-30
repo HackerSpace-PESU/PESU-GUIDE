4679200373515d3aabec70790b82f6da9ad0bb3e
Daniel <saldanha.dam@gmail.com>
2020-10-01 00:03:27 +0530

    mobile

--- script.js
+++ script.js
@@ -2,5 +2,7 @@ function changeDsc(header,scr,text){
     document.getElementById("dscHeader").innerText=header;
     document.getElementById("dscImage").src=scr;
     document.getElementById("dscText").innerHTML=text;
+
+    document.getElementsByClassName("dscCard")[0].classList.toggle('active');
     document.documentElement.scrollTop = 0;
 }
