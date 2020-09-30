9d0176997dc6bf09fff6d9d3973435f6a889bb6e
Daniel <saldanha.dam@gmail.com>
2020-09-30 19:52:13 +0530

    fix

--- script.js
+++ script.js
@@ -2,4 +2,5 @@ function changeDsc(header,scr,text){
     document.getElementById("dscHeader").innerText=header;
     document.getElementById("dscImage").src=scr;
     document.getElementById("dscText").innerHTML=text;
+    document.documentElement.scrollTop = 0;
 }
