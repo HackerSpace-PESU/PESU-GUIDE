02e44b044ce9a99ee3f85cfa3554f8e34e817255
Daniel <saldanha.dam@gmail.com>
2020-10-02 01:11:45 +0530

    comments

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
+
+//Function used to change the values in the description card
 function changeDsc(header,scr,map,text){
+
+    //To change the header
     document.getElementById("dscHeader").innerText=header;
+    //To change the image
     document.getElementById("dscImage").src=scr;
+    //To change the content test
     document.getElementById("dscText").innerHTML=text;
+    //To change the map image
     document.getElementById("dscMap").src=map;
 
+    //This is used to toggle the description card while in mobile view
     document.getElementsByClassName("dscCard")[0].classList.toggle('active');
+    //To bring the user to the top of the page after ther user clicks a collection card
     document.documentElement.scrollTop = 0;
 }
 
+//Function to close the description card in mobile view
 function closeDsc(){
     document.getElementsByClassName("dscCard")[0].classList.toggle('active');
 }
