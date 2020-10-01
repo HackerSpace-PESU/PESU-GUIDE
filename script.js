e64653e9d0d120eb80c26dff932bc70d53348d3c
Daniel <saldanha.dam@gmail.com>
2020-10-01 14:16:59 +0530

    Map functionality

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-function changeDsc(header,scr,text){
+function changeDsc(header,scr,map,text){
     document.getElementById("dscHeader").innerText=header;
     document.getElementById("dscImage").src=scr;
     document.getElementById("dscText").innerHTML=text;
+    document.getElementById("dscMap").src=map;
 
     document.getElementsByClassName("dscCard")[0].classList.toggle('active');
     document.documentElement.scrollTop = 0;
